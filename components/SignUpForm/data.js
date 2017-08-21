8563fc8f587de1a915abceae70591fa3bcb8e786
greatscot <greatscot001@gmail.com>
2017-08-21 13:52:03 +0100

    Remove exp from signup form and update schema to permit successfull user creation

--- components/SignUpForm/data.js
+++ components/SignUpForm/data.js
@@ -1,14 +1,58 @@
-import { graphql } from 'react-apollo';
+import { graphql, gql } from 'react-apollo';
 import { connect } from 'react-redux';
 import { dispatchers } from '../AuthFields/store';
-import createUserGql from './signupUser.gql';
+//import createUserGql from './signupUser.gql';
+
+const createUserGql = gql`mutation signupUser(
+  $firstName: String!
+  $lastName: String!
+  $email: String!
+  $password: String!
+  $occupation: String!
+  $username: String!
+  $location: String!
+  $exp: Int
+) {
+  createUser(
+    firstName: $firstName
+    lastName: $lastName
+	occupation: $occupation
+	username: $username
+	location: $location
+	exp: $exp
+    authProvider: { email: { email: $email, password: $password } }
+  ) {
+    id
+  }
+  signinUser(email: { email: $email, password: $password }) {
+    token
+  }
+}`
 
 const withMutation = graphql(createUserGql, {
   props: ({ mutate }) => ({
     mutations: {
-      signUp: ({ firstName, lastName, email, password, occupation, username, location, exp }) =>
+      signUp: ({
+        firstName,
+        lastName,
+        email,
+        password,
+        occupation,
+        username,
+        location,
+        exp
+      }) =>
         mutate({
-          variables: { firstName, lastName, email, password, occupation, username, location, exp }
+          variables: {
+            firstName,
+            lastName,
+            email,
+            password,
+            occupation,
+            username,
+            location,
+            exp
+          }
         })
     }
   })
