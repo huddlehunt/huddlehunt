e8cf1910f2e23f8f1245a26bb3d275ec76771b65
greatscot <greatscot001@gmail.com>
2017-09-20 08:56:06 +0100

    Update SignUp component to accept a profil picture and include the username in initialState after SignUp

--- components/AuthFields/index.js
+++ components/AuthFields/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Dropzone from 'react-dropzone';
 import PropTypes from 'prop-types';
 import { Main, SubmitButton } from './styles';
 
@@ -8,19 +9,43 @@ const AuthFields = props => {
     fields,
     handleTouch,
     handleChange,
+    handleFileDrop,
     handleSubmit,
     touched,
     errors
   } = props;
+  const onDrop = files => {
+    const data = new FormData();
+    data.append('data', files[0]);
+
+    fetch('https://api.graph.cool/file/v1/cj5o4cp4iwr8d0160vkfr1dk6', {
+      method: 'POST',
+      body: data
+    })
+      .then(response => response.json())
+      .then(file => {
+        handleFileDrop(file.url);
+      });
+  };
   const mapFields = fields.map(field =>
     <div key={field.key}>
-      <input
-        name={field.attr.name}
-        type={field.attr.type}
-        placeholder={field.attr.label}
-        onChange={handleChange}
-        onFocus={handleTouch}
-      />
+      {field.attr.type !== 'file' &&
+        <input
+          name={field.attr.name}
+          type={field.attr.type}
+          placeholder={field.attr.label}
+          onChange={handleChange}
+          onFocus={handleTouch}
+        />}
+      {field.attr.type === 'file' &&
+        <Dropzone
+          inputProps={field.attr}
+          onDrop={onDrop}
+          accept="image/*"
+          multiple={false}
+        >
+          <p>Drop your profile pic here, or click to browse and upload</p>
+        </Dropzone>}
       {errors &&
         <div>
           {errors[field.attr.name]}
@@ -54,6 +79,7 @@ AuthFields.propTypes = {
   fields: PropTypes.array.isRequired,
   handleTouch: PropTypes.func.isRequired,
   handleChange: PropTypes.func.isRequired,
+  handleFileDrop: PropTypes.func.isRequired,
   handleSubmit: PropTypes.func.isRequired,
   touched: PropTypes.bool.isRequired,
   errors: PropTypes.object.isRequired
