ecc4be3ace3adff5e23dfdf100f140fc47057b9d
greatscot <greatscot001@gmail.com>
2017-10-02 07:48:22 +0100

    added google map to huddleMap component, requires the google-maps-react package, which currently needs some manual fixes for isomorphic appssee https://github.com/fullstackreact/google-maps-react/issues/64

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import PostList from '../components/PostList';
+import HuddleMap from '../components/HuddleMap';
 import withData from '../libraries/withData';
 import DefaultCon from '../containers/Default';
 
 export default withData(props =>
   <DefaultCon {...props}>
     <PostList />
+    <HuddleMap />
   </DefaultCon>
 );
